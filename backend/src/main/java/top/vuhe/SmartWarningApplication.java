34fae29d74418abd9007850e7de6fcb744e6ee4d
vuhe <zhuhe202@qq.com>
2020-11-07 17:55:40 +0800

    feat(backend): 初始化后端框架
    从sping项目生成器获取、使用的模版，在此模版基础上进行后续开发

---
+++ backend/src/main/java/top/vuhe/SmartWarningApplication.java
@@ -0,0 +1,13 @@
+package top.vuhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWarningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartWarningApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/top/vuhe/backend/SmartWarningApplicationTests.java
@@ -0,0 +1,13 @@
+package top.vuhe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWarningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
