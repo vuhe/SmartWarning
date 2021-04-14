aeef9b58c02d8ce85077c76244c0f46d479def75
vuhe <zhuhe202@qq.com>
2021-04-14 18:50:19 +0800

    fix(netty): 修复字节码解码错误

--- backend-kt/src/main/kotlin/top/vuhe/sw/entity/equipment/bo/RealTimeBO.kt
+++ backend-kt/src/main/kotlin/top/vuhe/sw/entity/equipment/bo/RealTimeBO.kt
@@ -3,7 +3,6 @@ package top.vuhe.sw.entity.equipment.bo
 import top.vuhe.sw.entity.equipment.ElectricInfo
 import top.vuhe.sw.entity.equipment.dto.RealTimeDTO
 import top.vuhe.sw.entity.equipment.dto.StatusDTO
-import top.vuhe.sw.entity.equipment.dto.StatusPair
 import top.vuhe.sw.entity.equipment.dto.ThresholdDTO
 import java.util.*
 
@@ -11,6 +10,9 @@ class RealTimeBO(bytes: List<Byte>) : ElectricInfo {
     companion object {
         private const val HEAD_LEN = 2
         private const val ROW_LEN = 3
+        private val hasTowValue = setOf(
+            37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59
+        )
     }
 
     private val status: StatusDTO
@@ -22,22 +24,35 @@ class RealTimeBO(bytes: List<Byte>) : ElectricInfo {
         ) { "读取byte时出错" }
         val it = bytes.iterator()
         // 初始化状态List
-        val statusList: MutableList<StatusPair> = LinkedList()
-        statusList.add(StatusPair(51, it.next().toInt()))
+        val statusList: MutableMap<Int, Int> = HashMap(52)
+        statusList[100] = it.next().toInt() and 0xFF
         // 初始化值List
         val valueMap: MutableMap<Int, Double> = HashMap(52)
         // 设置信号强度
-        valueMap[52] = it.next().toDouble()
+        valueMap[101] = (it.next().toInt() and 0xff) * 1.0
         // 存储实时值
         val length: Int = (bytes.size - HEAD_LEN) / ROW_LEN
         for (i in 0 until length) {
-            val channel: Int = it.next().toInt()
-            var value: Int = it.next().toInt()
-            value = value shl 8 + it.next()
+            val channel: Int = it.next().toInt() and 0xFF
+            var value: Int = it.next().toInt() and 0xFF
+            value = value shl 8 + (it.next().toInt() and 0xFF)
+
             if (value == 0xFFFF) {
-                statusList.add(StatusPair(channel, 0))
+                statusList[channel] = 0
             } else {
                 valueMap[channel] = value * 1.0 / 10
+            }
+        }
+        // 合并高低位
+        for (ch in hasTowValue) {
+            if (valueMap[ch] != null) {
+                val value = valueMap[ch]!!
+                val nextVal = valueMap[ch + 1]!!
+                valueMap[ch] = value * 10_000 + nextVal
+                valueMap.remove(ch + 1)
+            }
+            if (statusList[ch] != null) {
+                statusList.remove(ch + 1)
             }
         }
         status = statusList
